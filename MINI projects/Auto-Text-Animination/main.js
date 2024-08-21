cd920b234aa2f808d6634b6d73d83bb46d693e89
NITHANR <nithanr39@gmail.com>
2024-08-21 21:09:41 +0530

    feat:added mr and mrs for auto test ani pro

--- MINI projects/Auto-Text-Animination/main.js
+++ MINI projects/Auto-Text-Animination/main.js
@@ -4,11 +4,15 @@ var charIndex=0;
 var nameIndex=0;
 updateText();
 function updateText(){
-    container.innerHTML = `<h1>I am 
+    container.innerHTML = `<h1>I am ${names[nameIndex].charAt(0)==='N'?"Mr.":"Mrs."  }
             ${names[nameIndex].slice(0,charIndex)}</h1>`;
     charIndex++;
     if(names[nameIndex].length===charIndex-1){
-        nameIndex++;
+        nameIndex++ ;
+        charIndex=0;
+    }
+    if(nameIndex=== names.length){
+        nameIndex=0;
         charIndex=0;
     }
     setTimeout(updateText,400);
