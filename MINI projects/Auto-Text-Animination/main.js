5b0f94217edcd0d3b915badaa384fd3fc4d2870e
NITHANR <nithanr39@gmail.com>
2024-08-21 20:58:09 +0530

    feat:Auto name effect

---
+++ MINI projects/Auto-Text-Animination/main.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container");
+const names = ["NITHAN","ISHANI"];
+var charIndex=0;
+var nameIndex=0;
+updateText();
+function updateText(){
+    container.innerHTML = `<h1>I am 
+            ${names[nameIndex].slice(0,charIndex)}</h1>`;
+    charIndex++;
+    if(names[nameIndex].length===charIndex-1){
+        nameIndex++;
+        charIndex=0;
+    }
+    setTimeout(updateText,400);
+};
