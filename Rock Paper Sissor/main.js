8e20f77b40ef1fa965e409ceed85aae539b99249
NITHANR <nithanr39@gmail.com>
2024-09-12 18:27:39 +0530

    rps game - html and css comp js srt

---
+++ Rock Paper Sissor/main.js
@@ -0,0 +1,7 @@
+const userScore =0;
+const computerScore =0;
+const userScore_span= document.getElementById("user-score");
+const computerScore_span= document.getElementById("computer-score");
+
+console.log(userScore_span);
+console.log(computerScore_span);
