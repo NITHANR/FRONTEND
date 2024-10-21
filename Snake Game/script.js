a614d3fee07b565822b7a82da674cde661c3a966
NITHANR <nithanr39@gmail.com>
2024-10-21 11:10:17 +0530

    snake game completed

--- Snake Game/script.js
+++ Snake Game/script.js
@@ -2,20 +2,26 @@
 const board = document.getElementById("game-board");
 const instructionText= document.getElementById("instruction-text");
 const logo  = document.getElementById("logo");
+const score = document.getElementById("score");
+const highScoreText = document.getElementById('highScore');
+
 
 const gridSize  = 20;
 let snake = [{'x':10,'y':10}];
 let food = generateFood();
+let highScore = 0;
 let direction = 'right';
 let gameInterval;
 let gameSpeedDelay = 200;
 let gameStarted = false;
+
 
 //draw game map,snake,food
 function draw(){
     board.innerHTML='';
     drawSnake();
     drawFood();
+    updateScore();
 }
 
 // create snake or food
@@ -47,9 +53,60 @@ function drawSnake(){
 }
 
 function drawFood(){
-    const foodElement = createGameElement('div','food');
-    setPosition(foodElement,food);
-    board.appendChild(foodElement);
+    if(gameStarted){
+        const foodElement = createGameElement('div','food');
+        setPosition(foodElement,food);
+        board.appendChild(foodElement);
+    }
+
+}
+
+// update score
+function updateScore(){
+    const curretScore = snake.length -1;
+    score.innerText = curretScore.toString().padStart(3,'0');
+}
+
+// stop game
+function stopGame(){
+    clearInterval(gameInterval);
+    gameStarted=false;
+    instructionText.style.display="block";
+    logo.style.display='block';
+
+}
+
+function updateHighScore(){
+    const curretScore = snake.length -1;
+    if(curretScore>highScore){
+        highScore  =curretScore;
+        highScoreText.textContent = highScore.toString().padStart(3,'0');
+    }
+    highScoreText.style.display='block';
+}
+
+// reset game
+function resetGame(){
+    updateHighScore();
+    stopGame();
+    snake = [{'x':10,'y':10}];
+    food = generateFood();
+    direction = 'right';
+    gameSpeedDelay =200;
+    updateScore();
+}
+
+// collusion chk
+function checkCollusion(){
+    const head = snake[0];
+    if(head.x < 1 || head.x > gridSize || head.y < 1 || head.y > gridSize){
+        resetGame();
+    }
+    for(let i =1;i<snake.length;i++){
+        if(head.x === snake[i].x && head.y === snake[i].y){
+            resetGame();
+        }
+    }
 }
 
 function move(){
@@ -76,7 +133,7 @@ function move(){
         clearInterval(gameInterval);//past intervals
         gameInterval=setInterval(()=>{
             move();
-            // checkCollusion()
+            checkCollusion()
             draw();
         },gameSpeedDelay);
     }
@@ -107,7 +164,7 @@ function startGame(){
     logo.style.display='none';
     gameInterval = setInterval(()=>{
         move();
-        // checkCollusion();
+        checkCollusion();
         draw();
     },gameSpeedDelay);
 
